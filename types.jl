a0d680ba42706eddc7ac9a04402efed308a81048
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2017-03-28 14:23:50 +0200

    some editions

--- types.jl
+++ types.jl
@@ -12,20 +12,23 @@ type Gene
 end
 
 type Chromosome
-    genes::Array{Gene}(1) # 1D array of genes
+    genes::Array{Gene,1} # 1D array of genes
     origin::Bool # parental origin of chromosome (paternal/maternal)
 end
 
 type Individual
-    genome::Array{Chromosome} # genome = array of chromosomes
+    genome::Array{Chromosome,2} # genome = 2D array of chromosomes (>=1 sets)
+    gamete::Array{Chromosome,2} # gamete = 2D array of chromosomes (>=0 sets)
+    fitness::Float # reproduction scaling factor representing life history
     stage::String # demographic stage of individual
 end
 
 
 ## methods:
 
-function mutate(gene::Gene)
-    for i in gene
+function mutate(gene::Gene, temp::Float, p::Float)
+    for i in eachindex(gene.sequence)
+        rand < temp*p && gene.sequence[i]=rand(collect("acgt"),1)[1] # for now, but consider indels!
     end
 end
 
