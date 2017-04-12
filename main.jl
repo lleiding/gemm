a6c5290379d366d2f4bbc0e8a372771602247375
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2017-04-12 15:18:48 +0200

    additional land, patch, ind cycles + comments

--- main.jl
+++ main.jl
@@ -5,6 +5,20 @@ using GeneInds
 
 using Distributions
 
+## main time cycle
 for t in 1:maxt
+    ## cycle land masses
+    for i in islands
+        ## cycle patches on land masses
+        for p in patches[i]
+            ## cycle individuals
+            for j in community
+            end
+            ## /ind cycle
+        end
+        ## /patch cycle
+    end
+    ## /island cycle
 end
+## /time cycle
 
