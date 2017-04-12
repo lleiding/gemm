0661f3bcd8eeffb710f8a90c0194c36e38224591
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2017-04-12 15:53:27 +0200

    lay out main structure of procedures

--- main.jl
+++ main.jl
@@ -12,7 +12,11 @@ for t in 1:maxt
         ## cycle patches on land masses
         for p in patches[i]
             ## cycle individuals
-            for j in community
+            for j in community # individuals are sorted according to their phenologies
+                germinate(community[j])
+                mature(community[j])
+                reproduce(community[j])
+                disperse(community[j])
             end
             ## /ind cycle
         end
