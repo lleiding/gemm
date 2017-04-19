cd3638ad2d8b113fe7b33efc9cdd1a737a55d7a6
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2017-04-19 11:48:37 +0200

    structure simulation run as function

--- main.jl
+++ main.jl
@@ -5,24 +5,25 @@ using GeneInds
 
 using Distributions
 
-## main time cycle
-for t in 1:maxt
-    ## cycle land masses
-    for i in islands
-        ## cycle patches on land masses
-        for p in patches[i]
-            ## cycle individuals
-            for j in community # individuals are sorted according to their phenologies
-                germinate(community[j])
-                mature(community[j])
-                reproduce(community[j])
-                disperse(community[j])
+function run_simulation(maxt::Int, islands::Array)
+    ## main time cycle
+    for t in 1:maxt
+        ## cycle land masses
+        for i in islands
+            ## cycle patches on land masses
+            for p in i.patches
+                ## cycle individuals
+                for j in p.community # individuals are sorted according to their phenologies
+                    germinate(j)
+                    mature(j)
+                    reproduce(j)
+                    disperse(j)
+                end
+                ## /ind cycle
             end
-            ## /ind cycle
+            ## /patch cycle
         end
-        ## /patch cycle
+        ## /island cycle
     end
-    ## /island cycle
+    ## /time cycle
 end
-## /time cycle
-
