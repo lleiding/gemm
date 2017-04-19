f971cff84beb58bf486c0053c4b6671bc4e3020d
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2017-04-19 15:26:11 +0200

    syimplification for developing, comments

--- types.jl
+++ types.jl
@@ -19,8 +19,8 @@ type Chromosome
 end
 
 type Individual
-    genome::Array{Chromosome,2} # genome = 2D array of chromosomes (>=1 sets)
-    gamete::Array{Chromosome,2} # gamete = 2D array of chromosomes (>=0 sets)
+    genome::Array{Chromosome,1} # genome = 2D array of chromosomes (>=1 sets)
+    gamete::Array{Chromosome,1} # gamete = 2D array of chromosomes (>=0 sets)
     fitness::Float64 # reproduction scaling factor representing life history
     stage::String # demographic stage of individual
     isnew::Bool # indicator whether individual is new to a patch
@@ -42,14 +42,17 @@ end
 
 function mutate(gene::Gene, temp::Float64, p::Float64)
     for i in eachindex(gene.sequence)
-        (rand < temp*p) && (gene.sequence[i]=rand(collect("acgt"),1)[1]) # for now, but consider indels!
+        (rand() < temp*p) && (gene.sequence[i]=rand(collect("acgt"),1)[1]) # for now, but consider indels!
     end
 end
 
 function recombinate(chromosome::Chromosome)
 end
 
+## following: act @ population/community level?
+
 function germinate(ind::Individual)
+    ind.isnew = false
     if ind.stage == "seed"
         ind.stage = "juvenile"
     end
@@ -67,5 +70,8 @@ function reproduce(ind::Individual)
 end
 
 function disperse(ind::Individual)
+    if !ind.isnew
+    end
+##    ind.isnew = true
 end
 
