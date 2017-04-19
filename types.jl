2be23df82011bfe078d6d6d9fe733ac8c265e3b7
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2017-04-19 13:50:19 +0200

    bug fixing

--- types.jl
+++ types.jl
@@ -10,7 +10,7 @@ using Distributions
 type Gene
     sequence::String # contains gene base code
     codes::String # function of the gene
-    value::Float # numerical effect of function
+    value::Float64 # numerical effect of function
 end
 
 type Chromosome
@@ -21,27 +21,28 @@ end
 type Individual
     genome::Array{Chromosome,2} # genome = 2D array of chromosomes (>=1 sets)
     gamete::Array{Chromosome,2} # gamete = 2D array of chromosomes (>=0 sets)
-    fitness::Float # reproduction scaling factor representing life history
+    fitness::Float64 # reproduction scaling factor representing life history
     stage::String # demographic stage of individual
     isnew::Bool # indicator whether individual is new to a patch
 end
 
 type Patch
-    community::Array{Individual,2} # holds the population (1D) of present species (2nd D)
-    altitude::Float # altitude: corresponds to T
-    nichea::Float # additional niches,
-    nicheb::Float # e.g. precipitation
+    community::Array{Individual,1} # holds the population (1D) of present species (2nd D)
+    altitude::Float64 # altitude: corresponds to T
+    nichea::Float64 # additional niches,
+    nicheb::Float64 # e.g. precipitation
 end
 
 type Island
+    patches::Array{Patch,1}
 end
 
 
 ## methods:
 
-function mutate(gene::Gene, temp::Float, p::Float)
+function mutate(gene::Gene, temp::Float64, p::Float64)
     for i in eachindex(gene.sequence)
-        rand < temp*p && gene.sequence[i]=rand(collect("acgt"),1)[1] # for now, but consider indels!
+        (rand < temp*p) && (gene.sequence[i]=rand(collect("acgt"),1)[1]) # for now, but consider indels!
     end
 end
 
