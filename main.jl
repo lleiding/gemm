cd66f14acedc5be97f507767c6467b1c13182a94
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2017-05-08 12:10:18 +0200

    bugfix

--- main.jl
+++ main.jl
@@ -19,7 +19,7 @@ function run_simulation(maxt::Int, islands::Array{Island,1})
                 println(size(p.community, 1)) # testing
                 for j in eachindex(p.community) # individuals are sorted according to their phenologies
                     counter += 1 # for testing
-                    j.isnew && evaluate_environment(p, p.community[j])
+                    p.community[j].isnew && evaluate_environment(p, p.community[j])
                     germinate(p.community[j])
                     mature(p.community[j])
                     children = reproduce(p.community[j])
