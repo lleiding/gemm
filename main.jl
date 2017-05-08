9c4bdff020e6f8a2ba582c0be8bcb3a15a06015b
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2017-05-08 10:24:49 +0200

    comments, spaces

--- main.jl
+++ main.jl
@@ -14,29 +14,29 @@ function run_simulation(maxt::Int, islands::Array{Island,1})
             for p in i.patches
                 ## cycle individuals
                 offspring = []
-                counter=0
-                println(size(p.community,1))
+                counter = 0
+                println(size(p.community, 1))
                 for j in p.community # individuals are sorted according to their phenologies
-                    counter+=1
-                    j.isnew && evaluate_environment(p,j)
+                    counter += 1 # for testing
+                    j.isnew && evaluate_environment(p, j)
                     germinate(j)
                     mature(j)
                     children=reproduce(j)
-                    (children!=nothing) && (append!(offspring,children))
+                    (children!=nothing) && (append!(offspring, children))
                     disperse(j)
-                    (counter>20) && (j.dead=true)
+                    (counter>20) && (j.dead=true) # for testing
                 end
-                deleteat!(p.community,find(x -> x.dead,p.community))
-                append!(p.community,offspring)
+                deleteat!(p.community, find(x -> x.dead, p.community))
+                append!(p.community, offspring)
                 ## /ind cycle
             end
             ## /patch cycle
         end
         ## /island cycle
-        (t%10 == 0) && println("t=",t," ##################")
+        (t%10 == 0) && println("t=", t, " ##################")
     end
     ## /time cycle
 end
 
-oneisland=[Island([Patch([Individual([],[],1,"seed",true,5,0.3,0.3,0.1,false)],1,1,1)])]
+oneisland=[Island([Patch([Individual([], [], 1, "seed", true, 5, 0.3, 0.3, 0.1, false)], 1, 1, 1)])]
 run_simulation(100, oneisland)
