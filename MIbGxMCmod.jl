1bb35ce873d9c7aa6be6989c25aa0ecbd5a0e3f0
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2017-08-30 16:39:38 +0200

    include constants in module

--- MIbGxMCmod.jl
+++ MIbGxMCmod.jl
@@ -16,6 +16,13 @@ export Patch, # types
     genesis, checkviability!, establish!, age!, grow!, disperse!, compete!, reproduce! # functions
 
 
+const boltz = 1.38064852e-23 #  J/K = m2⋅kg/(s2⋅K)
+const act = 1e-19 # activation energy /J, ca. 0.63eV - Brown et al. 2004
+const normconst = 1e10 # normalization constant to get biologically realistic orders of magnitude
+const mutscaling=50 #parse(ARGS[2])
+const sexualreproduction = true
+
+
 ## Types:
 #########
 
