b906a9c9642c94c44ba026b7a27caa83572fc2fb
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2017-08-31 16:15:26 +0200

    expand analysis, visualisation

--- main.jl
+++ main.jl
@@ -8,19 +8,16 @@
 include("MIbGxMCmod.jl")
 
 
-using MIbGxMCmod
+using MIbGxMCmod, Plots
 
 
-function simulation(world::Array{Patch,1}, timesteps::Int=1000)
-    for t in 1:timesteps
-        checkviability!(world)
-        establish!(world)
-        age!(world)
-        grow!(world)
-        disperse!(world)
-        compete!(world)
-        reproduce!(world)
+function readmapfile(filename::String)
+    mapstrings = String[]
+    open(filename) do file
+        mapstrings = readlines(file)
     end
+    mapentries = map(split,mapstrings)
+    filter(x->size(x,1)>0,mapentries)
 end
 
 function createworld(maptable::Array{Array{SubString{String},1},1})
@@ -28,11 +25,11 @@ function createworld(maptable::Array{Array{SubString{String},1},1})
     area = 100 # CAVE: just for now...
     for entry in maptable
         size(entry,1) < 3 && error("please check your map file for incomplete or faulty entries. \n
-        Each line must contain patch information with at least \n
-        \t - a unique integer ID, \n
-        \t - an integer x coordinate, \n
-        \t - an integer y coordinate, \n
-        separated by a whitespace character (<ID> <x> <y>).")
+            Each line must contain patch information with at least \n
+            \t - a unique integer ID, \n
+            \t - an integer x coordinate, \n
+            \t - an integer y coordinate, \n
+            separated by a whitespace character (<ID> <x> <y>).")
         id = parse(Int64, entry[1])
         xcord = parse(Int64, entry[2])
         ycord = parse(Int64, entry[3])
@@ -48,22 +45,55 @@ function createworld(maptable::Array{Array{SubString{String},1},1})
     world
 end
 
-function readmapfile(filename::String)
-    mapstrings = String[]
-    open(filename) do file
-        mapstrings = readlines(file)
+function visualisation(world::Array{Patch,1},firstplot::Bool)
+    xcords = map(x->Int(floor(x.location[1])),world)
+    ycords = map(x->Int(floor(x.location[2])),world)
+    values = map(x->size(x.community,1),world)
+    xmin = minimum(xcords)
+    xmax = maximum(xcords)
+    xshift = 1 - xmin
+    ymin = minimum(ycords)
+    ymax = maximum(ycords)
+    yshift = 1 - ymin
+    mat = zeros(length(ymin:ymax),length(xmin:xmax))
+    mat[:] = -10
+    for i in eachindex(values)
+        mat[ycords[i]+yshift,xcords[i]+xshift]=values[i]
     end
-    mapentries = map(split,mapstrings)
-    filter(x->size(x,1)>0,mapentries)
+    if firstplot
+        gr()
+        heatmap(mat,aspect_ratio=1,show=true)
+    else
+        heatmap!(mat,aspect_ratio=1,show=true)
+    end
 end
 
-function analysis()
+function analysis(world::Array{Patch,1})
+    for patch in world
+        print("Patch #",patch.id," @",patch.location," (",patch.isisland,"): \t")
+        println(size(patch.community,1))
+    end
+end
+
+function simulation(world::Array{Patch,1}, timesteps::Int=1000)
+    for t in 1:timesteps
+        checkviability!(world)
+        establish!(world)
+        age!(world)
+        grow!(world)
+        disperse!(world)
+        compete!(world)
+        reproduce!(world)
+        #        mod(timesteps,20) == 0 && analysis(world)
+        mod(timesteps,20) == 0 && visualisation(world,t==20)
+    end
 end
 
 function runit()
     maptable=readmapfile("mapfile")
     world=createworld(maptable)
     simulation(world)
+    analysis(world)
 end
 
 runit()
