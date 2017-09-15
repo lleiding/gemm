138637e5b7fc38ba6f02f2aca3b2b54e5ac726ab
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2017-09-15 10:01:18 +0200

    move stuff to main module, clean up

--- MIbGxMCmod.jl
+++ MIbGxMCmod.jl
@@ -9,7 +9,7 @@
 module MIbGxMCmod
 
 
-using Distributions
+using Distributions, Plots
 
 
 export Patch, # types
@@ -75,6 +75,204 @@ Patch(id,location) = Patch(id,location,298,100,false,0,0,Individual[],false)
 
 ## Methods/Functions:
 #####################
+
+function readmapfile(filename::String)
+    println("Reading file \"$filename\"...")
+    mapstrings = String[]
+    open(filename) do file
+        mapstrings = readlines(file)
+    end
+    mapstrings = filter(x->!all(isspace,x),mapstrings) # remove empty lines
+    mapstrings = filter(x->x[1]!='#',mapstrings) # remove comment lines
+    mapsubstrings = map(split,mapstrings)
+    mapentries = map(x->map(String,x),mapsubstrings)
+    timesteps = 0
+    try
+        timesteps = parse(Int,filter(x->size(x,1)==1,mapentries)[1][1])
+    catch
+        timesteps = 1000
+        warn("your mapfile \"$filename\" does not include timestep information. Assumed $timesteps timesteps.")
+    end
+    mapentries = filter(x->size(x,1)>1,mapentries)
+    return timesteps,mapentries
+end
+
+function createworld(maptable::Array{Array{String,1},1})
+    println("Creating world...")
+    world = Patch[]
+    area = 100 # CAVE: just for now...
+    for entry in maptable
+        size(entry,1) < 3 && error("please check your map file for incomplete or faulty entries. \n
+                Each line must contain patch information with at least \n
+                \t - a unique integer ID, \n
+                \t - an integer x coordinate, \n
+                \t - an integer y coordinate, \n
+                separated by a whitespace character (<ID> <x> <y>).")
+        id = parse(Int64, entry[1])
+        xcord = parse(Int64, entry[2])
+        ycord = parse(Int64, entry[3])
+        size(entry,1) > 3 ? temperature = parse(Float64, entry[4]) : temperature = 298.0
+        isisland = false
+        if size(entry,1) > 4 && contains(lowercase(entry[5]),"island")
+            isisland = true # islands do not receive an initial community
+        end
+        newpatch = Patch(id,(xcord,ycord),temperature,area,isisland)
+        if size(entry,1) > 5 && contains(lowercase(entry[6]),"isolated")
+            newpatch.isolated = true
+        end
+        !isisland && append!(newpatch.community,genesis())
+        push!(world,newpatch)
+    end
+    world
+end
+
+function updateworld!(world::Array{Patch,1},maptable::Array{Array{String,1},1}) #TODO: add functionality to remove patches!
+    println("Updating world...")
+    area = 100 # CAVE: just for now...
+    for entry in maptable
+        size(entry,1) < 3 && error("please check your map file for incomplete or faulty entries. \n
+                Each line must contain patch information with at least \n
+                \t - a unique integer ID, \n
+                \t - an integer x coordinate, \n
+                \t - an integer y coordinate, \n
+                separated by a whitespace character (<ID> <x> <y>).")
+        id = parse(Int64, entry[1])
+        xcord = parse(Int64, entry[2])
+        ycord = parse(Int64, entry[3])
+        size(entry,1) > 3 ? temperature = parse(Float64, entry[4]) : temperature = 298.0
+        isisland = false
+        if size(entry,1) > 4 && contains(lowercase(entry[5]),"island")
+            isisland = true # islands do not receive an initial community
+        end
+        isolated = false
+        if size(entry,1) > 5 && contains(lowercase(entry[6]),"isolated")
+            isolated = true
+        end
+        try
+            p = find(x->x.id==id,world)
+            world[p].location = (xcord,ycord)
+            world[p].altitude = temperature
+            world[p].isisland = isisland
+            world[p].isolated = isolated
+        catch
+            newpatch = Patch(id,(xcord,ycord),temperature,area,isisland)
+            newpatch.isolated = isolated
+            push!(world,newpatch)
+        end
+    end
+    world
+end
+
+function visualisation(world::Array{Patch,1},firstplot::Bool)
+    xcords = map(x->Int(floor(x.location[1])),world)
+    ycords = map(x->Int(floor(x.location[2])),world)
+    popsizes = map(x->size(x.community,1),world)
+    xmin = minimum(xcords)
+    xmax = maximum(xcords)
+    xshift = 1 - xmin
+    ymin = minimum(ycords)
+    ymax = maximum(ycords)
+    yshift = 1 - ymin
+    mat = zeros(length(ymin:ymax),length(xmin:xmax))
+    mat[:] = -10
+    for i in eachindex(popsizes)
+        mat[ycords[i]+yshift,xcords[i]+xshift]=popsizes[i]
+    end
+    if firstplot
+        gr()
+        heatmap(mat,aspect_ratio=1,show=true)
+    else
+        heatmap(mat,aspect_ratio=1,show=true)
+    end
+end
+
+function analysis(world::Array{Patch,1})
+    for patch in world
+        print("Patch #",patch.id," @",patch.location," (",patch.isisland,"): \t")
+        println(size(patch.community,1))
+    end
+end
+
+function dumpinds(world::Array{Patch,1},io::IO=STDOUT,sep::String="\t")
+    header = true
+    traitkeys = []
+    for patch in world
+        for ind in patch.community
+            if header
+                print(io, "id", sep)
+                print(io, "xloc", sep)
+                print(io, "yloc", sep)
+                print(io, "temp", sep)
+                print(io, "area", sep)
+                ## print(io, "nichea", sep)
+                ## print(io, "nicheb", sep)
+                print(io, "landtype", sep)
+                print(io, "isolation", sep)
+                print(io, "age", sep)
+                print(io, "new", sep)
+                print(io, "fitness", sep)
+                print(io, "size", sep)
+                traitkeys = keys(ind.traits)
+                for key in traitkeys
+                    print(io, key, sep)
+                end
+                println(io)
+                header = false
+            end
+            print(io, patch.id, sep)
+            print(io, patch.location[1], sep)
+            print(io, patch.location[2], sep)
+            print(io, patch.altitude, sep)
+            print(io, patch.area, sep)
+            ## print(io, patch.nichea, sep)
+            ## print(io, patch.nicheb, sep)
+            patch.isisland ? print(io, "island", sep) : print(io, "continent", sep)
+            patch.isolated ? print(io, "yes", sep) : print(io, "no", sep)
+            print(io, ind.age, sep)
+            ind.isnew ? print(io, "yes", sep) : print(io, "no", sep)
+            print(io, ind.fitness, sep)
+            print(io, ind.size, sep)
+            for key in traitkeys
+                print(io, ind.traits[key], sep)
+            end
+            println(io)
+        end
+    end
+end
+
+function writedata(world::Array{Patch,1}, seed::Int64, mapfile::String)
+    filename = mapfile * "_seed" * "$seed" * ".out"
+    counter = 0
+    extension = ""
+    while ispath(filename * extension)
+        extension = "_$counter"
+        counter += 1
+        counter > 9 && error("file \"$filename$extension\" exists. Please clear your directory.")
+    end
+    filename *= extension
+    touch(filename)
+    println("Writing data to \"$filename\"...")
+    open(filename, "w") do file
+        dumpinds(world,file)
+    end
+end
+
+function writerawdata(world::Array{Patch,1}, seed::Int64, mapfile::String, timestep::Int64)
+    filename = mapfile * "_seed" * "$seed" * "_t" * "$timestep" * ".jl"
+    counter = 0
+    extension = ""
+    while ispath(filename * extension)
+        extension = "_$counter"
+        counter += 1
+        counter > 9 && error("file \"$filename$extension\" exists. Please clear your directory.")
+    end
+    filename *= extension
+    touch(filename)
+    println("Writing data to \"$filename\"...")
+    open(filename, "w") do file
+        println(file,world)
+    end
+end
 
 function meiosis(genome::Array{Chromosome,1},maternal::Bool) # TODO: include further dynamics, errors...
     firstset = find(x->x.maternal,genome)
