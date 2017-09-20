3b9ac6faea3dd30dcb7f2bbc44f70e47ad0dfaac
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2017-09-20 17:21:12 +0200

    findposspartners function

--- MIbGxMCmod.jl
+++ MIbGxMCmod.jl
@@ -600,7 +600,17 @@ function compete!(world::Array{Patch,1})
 end
 
 function findposspartners(world::Array{Patch,1}, ind::Individual, location::Tuple{Int64, Int64})
+    ind.isnew = true
+    radius = ind.traits["repradius"] + 0.5 # to account for cell width ... or not??
+    coordinates = Tuple[]
+    for x in -radius:radius, y in -radius:radius
+        sqrt(x^2+y^2) <= radius && push!(coordinates,(x+location[1],y+location[2]))
+    end
+    map(x->checkborderconditions!(world,x[1],x[2]),coordinates)
     posspartners = Individual[]
+    map(x->append!(posspartners,x.community),filter(x->in(x.location,coordinates),world))
+    filter!(x->!x.isnew,posspartners) # filter out mating individual
+    ind.isnew = false
     posspartners
 end
 
@@ -749,6 +759,7 @@ function genesis(ninds::Int64=100, meangenes::Int64=meangenes,
                                                 "growthrate",
                                                 "mutprob",
                                                 "repprob",
+                                                "repradius",
                                                 "reprate",
                                                 "repsize",
                                                 "reptol",
