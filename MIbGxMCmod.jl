89465a6faa4fd4ad365c6a2a6d56e7f84b7781ef
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2017-10-09 16:53:28 +0200

    output dir in functs

--- MIbGxMCmod.jl
+++ MIbGxMCmod.jl
@@ -788,8 +788,8 @@ function setupdatadir(settings::Dict{String, Any})
     ?isdir(settings["dir"]) && mkdir(settings["dir"])
 end
 
-function writedata(world::Array{Patch,1}, seed::Int64, mapfile::String)
-    filename = mapfile * "_seed" * "$seed" * ".out"
+function writedata(world::Array{Patch,1}, seed::Int64, mapfile::String, settings::Dict{String, Any})
+    filename = settings["dir"] * "/" * mapfile * "_seed" * "$seed" * ".out"
     counter = 0
     extension = ""
     while ispath(filename * extension)
@@ -810,7 +810,7 @@ function writedata(world::Array{Patch,1}, seed::Int64, mapfile::String)
 end
 
 function writerawdata(world::Array{Patch,1}, mapfile::String, settings::Dict{String, Any}, timestep::Int64)
-    filename = mapfile * "_s" * "$(settings["seed"])" * "_$(settings["linkage"])" * "lnk" * "_$(settings["tolerance"])" * "tol" * "_t" * "$timestep" * ".jl"
+    filename = settings["dir"] * "/" * mapfile * "_s" * "$(settings["seed"])" * "_$(settings["linkage"])" * "lnk" * "_$(settings["tolerance"])" * "tol" * "_t" * "$timestep" * ".jl"
     counter = 0
     extension = ""
     while ispath(filename * extension)
@@ -828,7 +828,7 @@ end
 
 function recordcolonizers(colonizers::Array{Individual, 1}, mapfile::String, settings::Dict{String, Any}, timestep::Int64)
     record = (timestep, colonizers)
-    filename = mapfile * "_s" * "$(settings["seed"])" * "_$(settings["linkage"])" * "lnk" * "_$(settings["tolerance"])" * "tol" * "colonizers" * ".jl"
+    filename = settings["dir"] * "/" * mapfile * "_s" * "$(settings["seed"])" * "_$(settings["linkage"])" * "lnk" * "_$(settings["tolerance"])" * "tol" * "colonizers" * ".jl"
     touch(filename)
     println("Colonisation! Writing data to \"$filename\"...")
     open(filename, "a") do file
