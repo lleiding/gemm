0681f7d8fd604bf84ff481338b259ea5ab4734a4
Ludwig Leidinger <l.leidinger@gmx.net>
2017-11-20 14:59:45 +0100

    remove array info, include comments

--- run_scenarios.sh
+++ run_scenarios.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sbatch --array=3-6 islandsim_hf.sh
-sbatch --array=3-6 islandsim_hn.sh
-sbatch --array=3-6 islandsim_lf.sh
-sbatch --array=3-6 islandsim_ln.sh
+sbatch islandsim_hf.sh # high tol, full lnkg
+sbatch islandsim_hn.sh # high tol, no lnkg
+sbatch islandsim_lf.sh # low tol, full lnkg
+sbatch islandsim_ln.sh # low tol, no lnkg
