afd373494f736633326df2a6ba401d1069a7149c
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2017-11-29 16:23:28 +0100

    nichea values

--- MIbGxMCmod.jl
+++ MIbGxMCmod.jl
@@ -727,8 +727,11 @@ function createworld(maptable::Array{Array{String,1},1}, settings::Dict{String,A
         if size(entry,1) > 5 && contains(lowercase(entry[6]),"isolated")
             newpatch.isolated = true
         end
-        if size(entry,1) > 6
-            newpatch.nichea = parse(Float64, entry[7])
+        if size(entry,1) > 6 || settings["heterogeneity"] == "high"
+            try
+                newpatch.nichea = parse(Float64, entry[7])
+            catch
+                newpatch.nichea = rand([1:1000])
         end
         if size(entry,1) > 7
             newpatch.nicheb = parse(Float64, entry[8])
