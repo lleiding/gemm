66dea81c96a1be2dca69441017e84dd9cdf2b92c
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-01-08 16:22:04 +0100

    fix MTE rates

--- MIbGxMCmod.jl
+++ MIbGxMCmod.jl
@@ -21,8 +21,8 @@ export Patch, # types
 const boltz = 1.38064852e-23 # J/K = m2⋅kg/(s2⋅K)
 const act = 1e-19 # activation energy /J, ca. 0.63eV - Brown et al. 2004
 const growthrate = exp(25.2) # global base growth/biomass production from Brown et al. 2004
-const mortality = exp(10) # global base mortality from Brown et al. 2004
-const fertility = exp(26.0) # global base reproduction rate from Brown et al. 2004, alternatively 23.8
+const mortality = exp(26.3) # global base mortality from Brown et al. 2004
+const fertility = exp(23.8) # global base reproduction rate from Brown et al. 2004, alternatively 25.0
 const phylconstr = 50 #parse(ARGS[2])
 # const meangenes = 20 # mean number of genes per individual
 const mutationrate = 1e-3 * 0.3e11 # 1 base in 1000, correction factor for metabolic function
