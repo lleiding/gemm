26a9369d32ec7d20eb07aa4b4fc880c1f87c96b9
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-01-12 11:31:16 +0100

    stop growth when repsize reached

--- MIbGxMCmod.jl
+++ MIbGxMCmod.jl
@@ -320,7 +320,7 @@ function grow!(patch::Patch) # WORK IN PROGRESS
         elseif !patch.community[idx].isnew
             repsize = patch.community[idx].traits["repsize"]
             mass = patch.community[idx].size
-            if true #mass < repsize # stop growth if reached repsize 
+            if mass <= repsize # stop growth if reached repsize 
                 growth = growthrate * patch.community[idx].fitness * mass^(3/4) * exp(-act/(boltz*temp))
                 newmass = mass + growth
                 if newmass > 0 && mass > 0
