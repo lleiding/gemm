219d06812d1362d25df8d9176eed5f0b42803628
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-01-16 14:33:39 +0100

    comment

--- MIbGxMCmod.jl
+++ MIbGxMCmod.jl
@@ -21,7 +21,7 @@ export Patch, # types
 const boltz = 1.38064852e-23 # J/K = m2⋅kg/(s2⋅K)
 const act = 1e-19 # activation energy /J, ca. 0.63eV - Brown et al. 2004
 const growthrate = exp(25.2) # global base growth/biomass production from Brown et al. 2004
-const mortality = exp(20) # global base mortality from Brown et al. 2004 is 26.3, but competition introduces add. mort.
+const mortality = exp(20) # global base mortality from Brown et al. 2004 is 26.3, but competition and dispersal introduce add. mort.
 const fertility = exp(23.8) # global base reproduction rate from Brown et al. 2004, alternatively 25.0
 const phylconstr = 50 #parse(ARGS[2])
 # const meangenes = 20 # mean number of genes per individual
