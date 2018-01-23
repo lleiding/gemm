1900f6206f6d8f9e14fdffcae9ef5d009d6ec7c1
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-01-23 14:01:06 +0100

    restrict body masses to realistic range

--- MIbGxMCmod.jl
+++ MIbGxMCmod.jl
@@ -542,11 +542,11 @@ end
 
 function createtraits(traitnames::Array{String,1}, settings::Dict{String,Any}) #TODO: this is all very ugly. (case/switch w/ v. 2.0+?)
     traits = Trait[]
-    seedsize = exp(-20 + 19 * rand()) # smaller range for seeds
-    repsize = exp(-20 + 20 * rand())
+    seedsize = exp(-20 + 12 * rand()) # smaller range for seeds
+    repsize = exp(-10 + 15 * rand())
     while repsize <= seedsize
-        seedsize = exp(-20 + 19 * rand())
-        repsize = exp(-20 + 20 * rand())
+        seedsize = exp(-20 + 12 * rand())
+        repsize = exp(-10 + 15 * rand())
     end
     for name in traitnames
         if contains(name,"rate")
