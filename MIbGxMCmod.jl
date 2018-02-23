36e10b8847a628f3fec0ea312c6d7c3b22aa3955
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-02-23 14:20:38 +0100

    fix negative values for Normal dist in mutate

--- MIbGxMCmod.jl
+++ MIbGxMCmod.jl
@@ -150,8 +150,8 @@ function mutate!(ind::Individual, temp::Float64, settings::Dict{String,Any})
                     for trait in chrm.genes[idx].codes
                         (contains(trait.name, "mutprob") && mutationrate != 0) && continue
                         contains(trait.name, "reptol") && settings["tolerance"] != "evo" && continue # MARK CAVE!
-                        while trait.value == 0
-                            trait.value = rand(Normal(0,0.01))
+                        while trait.value <= 0
+                            trait.value = abs(rand(Normal(0,0.01)))
                         end
                         newvalue = trait.value + rand(Normal(0, trait.value/phylconstr)) # CAVE: phylconstr! new value for trait
                         newvalue < 0 && (newvalue=abs(newvalue))
