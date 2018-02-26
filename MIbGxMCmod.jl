ed68ba924e23ab9f5bf27989118c0ad5acd7a386
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-02-26 16:03:56 +0100

    decrease fertility, niches random

--- MIbGxMCmod.jl
+++ MIbGxMCmod.jl
@@ -22,7 +22,7 @@ const boltz = 1.38064852e-23 # J/K = m2⋅kg/(s2⋅K)
 const act = 1e-19 # activation energy /J, ca. 0.63eV - Brown et al. 2004
 const growthrate = exp(25.2) # global base growth/biomass production from Brown et al. 2004
 const mortality = exp(20.6) # global base mortality from Brown et al. 2004 is 26.3, but competition and dispersal introduce add. mort.
-const fertility = exp(28.0) # global base reproduction rate 23.8 from Brown et al. 2004, alternatively 25.0
+const fertility = exp(26.0) # global base reproduction rate 23.8 from Brown et al. 2004, alternatively 25.0
 const phylconstr = 10 #parse(ARGS[2])
 # const meangenes = 20 # mean number of genes per individual
 const mutationrate = 1e-3 * 0.3e11 # 1 base in 1000, correction factor for metabolic function
@@ -573,7 +573,7 @@ function createtraits(traitnames::Array{String,1}, settings::Dict{String,Any}) #
         elseif contains(name,"dispshape")
             push!(traits, Trait(name, rand() * maxdispmean))
         elseif contains(name, "tempopt")
-            push!(traits,Trait(name, 298.0))#(rand() * 60) + 263)) #CAVE: code values elsewhere?
+            push!(traits,Trait(name, rand() * 60 + 263)) #CAVE: code values elsewhere?
         elseif contains(name, "temptol")
             push!(traits,Trait(name,rand()*10)) #CAVE: code values elsewhere?
         elseif contains(name, "mut")
@@ -591,7 +591,7 @@ function createtraits(traitnames::Array{String,1}, settings::Dict{String,Any}) #
         elseif contains(name, "seedsize")
             push!(traits,Trait(name,seedsize)) #CAVE: code values elsewhere?
         elseif contains(name, "precopt")
-            push!(traits, Trait(name, 500))#rand(0:1000)))
+            push!(traits, Trait(name, rand(0:1000)))
         elseif contains(name, "prectol")
             push!(traits, Trait(name, rand(0:500)))
         else
