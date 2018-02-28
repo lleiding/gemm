768925cf08c859fbe6a0a36c6e7af5b5f81e1236
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-02-28 16:58:18 +0100

    comment

--- MIbGxMCmod.jl
+++ MIbGxMCmod.jl
@@ -154,7 +154,7 @@ function mutate!(ind::Individual, temp::Float64, settings::Dict{String,Any})
                         while trait.value <= 0
                             trait.value = abs(rand(Normal(0,0.01)))
                         end
-                        newvalue = trait.value + rand(Normal(0, trait.value/phylconstr)) # CAVE: phylconstr! new value for trait
+                        newvalue = trait.value + rand(Normal(0, trait.value/phylconstr)) # CAVE: maybe handle temp + prec separately
                         newvalue < 0 && (newvalue=abs(newvalue))
                         (newvalue > 1 && contains(trait.name,"prob")) && (newvalue=1)
                         while newvalue <= 0 #&& contains(trait.name,"mut")
