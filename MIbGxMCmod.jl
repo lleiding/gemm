2552f76dc890e65808fcb85df213d260e035fd40
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-03-14 17:19:06 +0100

    fix initial value of compat trait

--- MIbGxMCmod.jl
+++ MIbGxMCmod.jl
@@ -652,7 +652,7 @@ function creategenes(ngenes::Int64,traits::Array{Trait,1})
     if !any(map(x -> length(x.codes) == 0, genes)) # make sure there is a neutral gene!
         push!(genes, Gene(String(rand(collect("acgt"), genelength)), Trait[]))
     end
-    push!(genes, Gene(String(rand(collect("acgt"), genelength)), [Trait("compat", 0.0)])) # create extra compatibility gene
+    push!(genes, Gene(String(rand(collect("acgt"), genelength)), [Trait("compat", 1.0)])) # create extra compatibility gene
     genes
 end
 
