80df7974b4d266db3ea4f40f315795b91493839d
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-04-17 14:07:44 +0200

    update comment info

--- islandsim.jl
+++ islandsim.jl
@@ -1,15 +1,15 @@
 #!/usr/bin/env julia
-## Metabolic Individual-based Genetically-explicit Meta-Community Model
-## Ludwig Leidinger 2017
-## <l.leidinger@gmx.net>
-##
-## Main simulation
-##
-## For a list of options, run julia main.jl --help
-##
-## <MAPFILE> is a textfile containing information about the simulation arena
-## Every line describes one patch in the following format:
-## <ID> <X-COORDINATE> <Y-COORDINATE> [<TYPE>]
+
+# Island speciation model using GeMM
+#
+# Ludwig Leidinger 2018
+# <l.leidinger@gmx.net>
+#
+# For a list of options, run `julia islandsim.jl --help`
+#
+# <MAPFILE> is a textfile containing information about the simulation arena
+# Every line describes one patch in the following format:
+# <ID> <X-COORDINATE> <Y-COORDINATE> [<TYPE>]
 
 thisDir = pwd() * "/src"
 any(path -> path == thisDir, LOAD_PATH) || push!(LOAD_PATH, thisDir)
