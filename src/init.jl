60cdfc6a92af9167412ecb5691d1b3905b1e4b6d
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-04-18 13:46:13 +0200

    change some println to info

--- src/init.jl
+++ src/init.jl
@@ -47,7 +47,7 @@ function genesis(settings::Dict{String,Any},
 end
 
 function createworld(maptable::Array{Array{String,1},1}, settings::Dict{String,Any})
-    println("Creating world...")
+    info("Creating world...")
     world = Patch[]
     length(maptable) > 1 ? area = cellsize : area = 1
     for entry in maptable
@@ -86,7 +86,7 @@ separated by a whitespace character (<ID> <x> <y>).")
 end
 
 function updateworld!(world::Array{Patch,1},maptable::Array{Array{String,1},1}) #TODO: add functionality to remove patches!
-    println("Updating world...")
+    info("Updating world...")
     area = 1 # CAVE: just for now...
     for entry in maptable
         size(entry,1) < 3 && error("please check your map file for incomplete or faulty entries. \n
