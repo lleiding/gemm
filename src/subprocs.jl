ed8fec20ded046bc9035ebbdcb18e6761afc804c
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-04-23 17:15:35 +0200

    adapt iscompatible to traitdict architecture

--- src/subprocs.jl
+++ src/subprocs.jl
@@ -132,18 +132,18 @@ function checkborderconditions!(world::Array{Patch,1},xdest::Float64,ydest::Floa
     xdest,ydest
 end
 
-function iscompatible(mate::Individual, ind::Individual)
+function iscompatible(mate::Individual, ind::Individual) # TODO: instead of concatenation, choose one compatibility sequence
     tolerance = ind.traits["reptol"]
     indgene = ""
     for chrm in ind.genome
         for gene in chrm.genes
-            any(x -> x.name == "compat", gene.codes) && (indgene *= gene.sequence)
+            any(x -> contains(x, "compat"), gene.codes) && (indgene *= gene.sequence)
         end
     end
     mategene = ""
     for chrm in mate.genome
         for gene in chrm.genes
-            any(x -> x.name == "compat", gene.codes) && (mategene *= gene.sequence)
+            any(x -> contains(x, "compat"), gene.codes) && (mategene *= gene.sequence)
         end
     end
     basediffs = 0
