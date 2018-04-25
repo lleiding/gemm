782f7fae52519a1159996f26ad3dd16e7a374d9e
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-04-25 11:16:55 +0200

    IMPORTANT FIX: wrong checking of traitsexist

--- src/subprocs.jl
+++ src/subprocs.jl
@@ -184,7 +184,7 @@ function findposspartners(world::Array{Patch,1}, ind::Individual, location::Tupl
         for mate in community
             mate.age == 0 && continue
             mate.lineage != ind.lineage && continue
-            traitsexist(mate, ["repsize"]) && continue
+            !traitsexist(mate, ["repsize"]) && continue
             mate.size < mate.traits["repsize"] && continue
             mate.isnew && continue
             !iscompatible(mate, ind) && continue
