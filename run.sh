a46a55f30aa6c8964c22f99cb42fd4b3cda147d8
Daniel Vedder <d.vedder@web.de>
2018-04-30 16:18:03 +0200

    Added config file parsing.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Starting simulation run."
-julia islandsim.jl --maps invasion.map --linkage "none" --nniches 2 --tolerance "none" --dest tests
+time julia islandsim.jl --config test.config
 echo "Done."
