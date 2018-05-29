b9f56884f2e3bd33a3a0fc6c85db29d3efbb88a4
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-05-29 15:03:51 +0200

    parse settings

--- src/mainprocs.jl
+++ src/mainprocs.jl
@@ -34,7 +34,7 @@ function mutate!(ind::Individual, temp::Float64, settings::Dict{String,Any})
             chrm.genes[idx].sequence = seq2num(String(charseq))
         end
     end
-    ind.traits = chrms2traits(ind.genome)
+    ind.traits = chrms2traits(ind.genome, settings)
 end
 
 function mutate!(patch::Patch, settings::Dict{String,Any})
