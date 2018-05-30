b3c28d82e8d5a484b91088041404525de4461024
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-05-30 17:00:49 +0200

    declare settings as const Dict

--- src/constants.jl
+++ src/constants.jl
@@ -1,5 +1,8 @@
 # Constants and settings for GeMM
 
+const settings = Dict{String, Any}()
+
+### below variables should become settings elements at some point
 const boltz = 1.38064852e-23 # J/K = m2⋅kg/(s2⋅K)
 const act = 1e-19 # activation energy /J, ca. 0.63eV - Brown et al. 2004
 const growthrate = exp(25.2) # global base growth/biomass production from Brown et al. 2004
