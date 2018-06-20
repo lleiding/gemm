29a2d4b90e5e2f04d57711687bedd65c9f36b4e6
Daniel Vedder <d.vedder@web.de>
2018-06-20 14:22:53 +0200

    Added plotTraits() to analyse.R

--- analyse.R
+++ analyse.R
@@ -12,26 +12,40 @@ if (!(file.exists(outdir) && file.info(outdir)$isdir)) {
 }
 
 
-# Plot the population sizes and diversity indices over time
-visualize = function(logfile="diversity.log", toFile=TRUE) {
+# Plot the distribution of traits in the population at the given timestep (-1 => END)
+plotTraits = function(timestep=-1, toFile=TRUE) {
+    traitfile = grep(paste("t", timestep, sep=""), list.files(outdir), value=T)
+    traitfilepath = paste(outdir, traitfile, sep="/")
+    ts = read.table(traitfilepath, header=T)
+    jpeg(paste(outdir, "/", simname, "_traits.jpg", sep=""), height=720, width=1800)
+    boxplot(ts$fitness*10, log(ts$size), log(ts$seedsize), log(ts$repsize), ts$lnkgunits/10,
+            ts$ngenes/10, ts$temptol, ts$tempopt/100, ts$prectol, ts$precopt, ts$compat*10,
+            ts$repradius*10, ts$dispmean, ts$dispshape,
+            names=c("Fitness (x10)", "log(Size)", "log(Seed size)", "log(Reprod. size)",
+                    "Chromosome (x0.1)", "Genes (x0.1)", "Temp. tolerance",
+                    "Temp. opt. (x0.01)", "Precip. tolerance",
+                    "Precip. optimum", "Compatibility (x10)", "Rep. radius (x10)",
+                    "Dispersal mean", "Dispersal shape"))
+    legend("top", c(paste("N = ", length(ts$counter))))
+    dev.off()
+}
+
+# Plot population size and diversity indices over time
+plotDiversity = function(logfile="diversity.log") {
     logfile = paste(outdir, logfile, sep="/")
     data = read.csv(logfile)
     data$lineages = data$lineages/10 #otherwise the Y axis is too big
     # Plot population sizes
-    if (toFile) {
-        jpeg(paste(outdir, "/", simname, "_population.jpg", sep=""), height=720,
-             width=length(data$population)*(2000/length(data$population)))
-    }
+    jpeg(paste(outdir, "/", simname, "_population.jpg", sep=""), height=720,
+         width=length(data$population)*(2000/length(data$population)))
     plot(data$population, xlab="Time", ylab="Population size", ylim=c(0, max(data$population)),
          col="red", type='l')
-    if (toFile) dev.off()
+    dev.off()
     # Plot diversity development
     ymax = max(data$lineages, data$alpha, data$beta, data$gamma, data$freespace)
-    if (toFile) {
-        jpeg(paste(outdir, "/", simname, "_diversity.jpg", sep=""), height=720,
-             width=length(data$population)*(2000/length(data$population)))
-    }
-    plot(data$lineages, col="orange", type='l', lty=2, ylim=c(0,ymax*1.5),
+    jpeg(paste(outdir, "/", simname, "_diversity.jpg", sep=""), height=720,
+         width=length(data$population)*(2000/length(data$population)))
+    plot(data$lineages, col="orange", type='l', lty=2, ylim=c(0,ymax*1.2),
          xlab="Time", ylab="Diversity")
     lines(data$freespace, col="cyan", type='l', lty=2)
     lines(data$alpha, col="blue", type='l')
@@ -40,7 +54,12 @@ visualize = function(logfile="diversity.log", toFile=TRUE) {
     legend("topright", c("Lineages (x 0.1)", "Free space per tile", "Alpha diversity",
                          "Beta diversity", "Gamma diversity"),
            col=c("orange", "cyan", "blue", "green", "red"), lwd=2)
-    if (toFile) dev.off()
+    dev.off()
+}
+
+visualize = function(toFile=TRUE) {
+    plotDiversity()
+    plotTraits()
 }
 
 visualize()
