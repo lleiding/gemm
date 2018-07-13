d1b6485fd7a0bff15f64c86e4de399d5b36d9c4e
Daniel Vedder <d.vedder@web.de>
2018-07-13 14:34:28 +0200

    Changed shuffle!() to rand() in findposspartner()
    Also changed findposspartners() reference to singular.

--- src/mainprocs.jl
+++ src/mainprocs.jl
@@ -314,11 +314,9 @@ function reproduce!(world::Array{Patch,1}, patch::Patch) #TODO: refactorize!
                     idx += 1
                     continue
                 end
-                posspartners = findposspartners(world, patch.community[idx], patch.location) # this effectively controls frequency of reproduction
+                partner = findposspartner(patch, patch.community[idx])
                 # <<< XXX There's a bug in here somewhere - "come out, come out, where ever you are!"
-                # length(posspartners) == 0 && push!(posspartners, patch.community[idx]) # selfing if no partners # CAVE!
-                if length(posspartners) > 0
-                    partner = rand(posspartners)
+                if partner != nothing
                     parentmass = currentmass - noffs * seedsize # subtract offspring mass from parent
                     if parentmass <= 0
                         idx += 1 #splice!(patch.community, idx)
