187a52d70cfc5dc277ea96950a5f1660dceadac8
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-08-23 15:19:26 +0200

    use setting fixtol in mutate!()

--- src/mainprocs.jl
+++ src/mainprocs.jl
@@ -4,7 +4,7 @@ function mutate!(traits::Array{Trait, 1}, settings::Dict{String, Any})
     for trait in traits
         traitname = settings["traitnames"][trait.nameindex]
         (contains(traitname, "mutprob") && mutationrate != 0) && continue
-        contains(traitname, "reptol") && continue # MARK CAVE!
+        contains(traitname, "reptol") && settings["fixtol"] && continue # MARK CAVE!
         oldvalue = trait.value
         contains(traitname, "tempopt") && (oldvalue -= 273)
         while oldvalue <= 0 # make sure sd of Normal dist != 0
