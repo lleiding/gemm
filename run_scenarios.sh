ef51c8b687d22bc5b0e1aec8209e0741193df24c
Ludwig Leidinger <l.leidinger@gmx.net>
2018-11-28 11:15:43 +0100

    clean up

--- run_scenarios.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sbatch islandsim_hf.sh # high tol, full lnkg
-sbatch islandsim_hn.sh # high tol, no lnkg
-sbatch islandsim_lf.sh # low tol, full lnkg
-sbatch islandsim_ln.sh # low tol, no lnkg
