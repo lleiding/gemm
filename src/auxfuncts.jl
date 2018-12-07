c1d48703622ca445ce9baa113f719a1be3fdb4a1
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2018-12-07 14:31:51 +0100

    fix missing brackets

--- src/auxfuncts.jl
+++ src/auxfuncts.jl
@@ -330,7 +330,7 @@ function createtraits(settings::Dict{String, Any}) #TODO: this is all very ugly.
         elseif occursin("precopt", traitnames[idx])
             push!(traits, Trait(idx, rand() * settings["precrange"]))
         elseif occursin("prectol", traitnames[idx])
-            push!(traits, Trait(idx, rand(Normal()))
+            push!(traits, Trait(idx, rand(Normal())))
         elseif occursin("repsize", traitnames[idx])
             push!(traits, Trait(idx, repsize))
         elseif occursin("reptol", traitnames[idx])
@@ -340,7 +340,7 @@ function createtraits(settings::Dict{String, Any}) #TODO: this is all very ugly.
         elseif occursin("tempopt", traitnames[idx])
             push!(traits, Trait(idx, settings["mintemp"] + rand() * tempoffset))
         elseif occursin("temptol", traitnames[idx])
-            push!(traits, Trait(idx, rand(Normal()))
+            push!(traits, Trait(idx, rand(Normal())))
         else
             push!(traits, Trait(idx, rand()))
         end
