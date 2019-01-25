0d349c8670bec31eb2d20d687a049b108cda021e
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2019-01-25 14:41:29 +0100

    restructure run_simulation as preparation for parallel simulation runs

--- run_simulation.jl
+++ run_simulation.jl
@@ -23,27 +23,26 @@ using
     GeMM,
     Random
 
-function runit(prerun::Bool = false)
+function runsim(config::String = "", seed::Integer = 0, prerun::Bool = false)
     settings = getsettings()
     if prerun
         settings = defaultSettings()
         settings["cellsize"] = 1.0e6
         settings["indsize"] = "adult"
         settings["popsize"] = "metabolic"
-        settings["maps"] = [getsettings()["maps"][1]]
+        settings["maps"] = [""]
         settings["phylo"] = false
+        settings["fasta"] = false
+        settings["raw"] = false
         settings["quiet"] = true
         settings["static"] = false
+        settings["seed"] = 1
     end
     Random.seed!(settings["seed"])
     !prerun && setupdatadir(settings)
     world = Patch[]
     for i in 1:length(settings["maps"])
         timesteps, maptable = readmapfile(settings["maps"][i], settings)
-        if prerun
-            timesteps = 10
-            maptable = [["1", "1", "1", "initpop"], ["2", "2", "1"]]
-        end
         i == 1 && (world = createworld(maptable, settings))
         i > 1 && updateworld!(world,maptable, settings)
         !prerun && writedata(world, settings, 0)
@@ -51,8 +50,12 @@ function runit(prerun::Bool = false)
     end
 end
 
-# compilation run:
-runit(true)
 
-# run intended simulation:
-runit()
+function rungemm(config::String = "", seed::Integer = 0)
+    # compilation run:
+    runsim("", 1, true)
+    # run intended simulation:
+    runsim(config, seed)
+end
+
+rungemm()
